f4798b5e8065ff5d09d378c368628183d095d112
sveinnmani <sveinnmani@hotmail.com>
2017-11-08 08:29:04 +0000

    Update verkefni_5.c

--- Verkefni_05/verkefni_5.c
+++ Verkefni_05/verkefni_5.c
@@ -10,20 +10,14 @@
 |*                             	    - Triple Sensor Line Tracking -                                   *|
 |*                                      ROBOTC on VEX 2.0 CORTEX                                      *|
 |*                                                                                                    *|
-|*  This program uses 3 VEX Line Follower Sensors to track a black line on a light(er) surface.       *|
-|*  There is a two second pause at the beginning of the program.                                      *|
+|*  I thessu forriti notar velmennid thrja lineFollowera til ad elta svarta linu sem er a brautinni   *|
+|*  Thegar ytt er a takka 7u a fjarstyringunni eda bumperswitch a velmenninu slekkur forritid a ser   *|
 |*                                                                                                    *|
-|*                                        ROBOT CONFIGURATION                                         *|
-|*    NOTES:                                                                                          *|
-|*    1)  Reversing 'rightMotor' (port 2) in the "Motors and Sensors Setup" is needed with the        *|
-|*        "Squarebot" mode, but may not be needed for all robot configurations.                       *|
-|*    2)  Lighting conditions, line darkness, and surface lightness change from place to place,       *|
-|*        so the value of 'threshold' may need to be changed to better suit your environment.         *|
 |*                                                                                                    *|
 |*    MOTORS & SENSORS:                                                                               *|
 |*    [I/O Port]          [Name]              [Type]                [Description]                     *|
-|*    Motor  - Port 2     rightMotor          VEX 3-wire module     Right side motor                  *|
-|*    Motor  - Port 3     leftMotor           VEX 3-wire module     Left side motor                   *|
+|*    Motor  - Port 2     leftMotor          VEX 3-wire module      Right side motor                  *|
+|*    Motor  - Port 3     rightMotor           VEX 3-wire module    Left side motor                   *|
 |*    Analog - Port 1     lineFollowerRIGHT   VEX Light Sensor      Front-right, facing down          *|
 |*    Analog - Port 2     lineFollowerCENTER  VEX Light Sensor      Front-center, facing down         *|
 |*    Analog - Port 3     lineFollowerLEFT    VEX Light Sensor      Front-left, facing down           *|
